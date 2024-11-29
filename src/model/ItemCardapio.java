02a47402a3c2aa6ff62f64710d26761b19681926
gabrielcouto <goldevil2015@gmail.com>
2024-11-29 15:08:44 -0300

    build: remove useless methods at itemCardapio

--- src/model/ItemCardapio.java
+++ src/model/ItemCardapio.java
@@ -1,58 +1,14 @@
 package model;
 
-import dataBase.DataBaseConnection;
 import dataBase.*;
 
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 // Classe model.ItemCardapio
 public class ItemCardapio {
     static Scanner scanner = new Scanner(System.in);
-
-    //estava em menu
-    public static List<ItemCardapio> cardapio = new ArrayList<>();
-
-
-    private int idItem;
-    private String nome;
-    private String descricao;
-    private double preco;
-
-    public ItemCardapio(int idItem, String nome, String descricao, double preco) {
-        this.idItem = idItem;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.preco = preco;
-    }
-
-    public int getIdItem() {
-        return idItem;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void alterarPreco(double novoPreco) {
-        this.preco = novoPreco;
-    }
-
-    public void alterarDescricao(String novaDescricao) {
-        this.descricao = novaDescricao;
-    }
-
 
     //switch 6 no menu
     public static void gerenciarCardapio() throws SQLException {
@@ -66,7 +22,6 @@ public class ItemCardapio {
                 String descricao = scanner.nextLine();
                 System.out.print("Digite o preço do item: ");
                 double preco = scanner.nextDouble();
-                cardapio.add(new ItemCardapio(cardapio.size() + 1, nome, descricao, preco));
                 InsertData.insertDataCardapio(0,nome, descricao, preco);
                 System.out.println("Item adicionado ao cardápio.");
                 scanner.nextLine();
